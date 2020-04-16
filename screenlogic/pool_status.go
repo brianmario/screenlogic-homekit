3f9c8d9c6df1008b6dc3bb7ac4dfd3cbd20cb63c
Brian Lopez <seniorlopez@gmail.com>
2020-04-16 10:30:11 -0700

    add some more shortcut methods for PoolStatus

--- screenlogic/pool_status.go
+++ screenlogic/pool_status.go
@@ -6,6 +6,26 @@ type PoolStatus struct {
 	protocol.PoolStatusResponsePacket
 }
 
+func (ps *PoolStatus) AmbientAirTemp() uint32 {
+	return ps.AirTemp
+}
+
+func (ps *PoolStatus) PoolWater() *protocol.BodyOfWater {
+	if len(ps.Bodies) == 0 {
+		return nil
+	}
+
+	return &ps.Bodies[BodyOfWaterPool]
+}
+
+func (ps *PoolStatus) SpaWater() *protocol.BodyOfWater {
+	if len(ps.Bodies) < 2 {
+		return nil
+	}
+
+	return &ps.Bodies[BodyOfWaterSpa]
+}
+
 func (ps *PoolStatus) IsReady() bool {
 	return ps.OK == 1
 }
