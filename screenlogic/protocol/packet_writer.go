568fbd4f09fd3f525b43a94be0382fea9948f052
Brian Lopez <seniorlopez@gmail.com>
2020-04-24 23:13:28 -0700

    Make PacketWriter use new memory every time, for now
    What I'm removing was probably fine, but I'll come back and
    optimize later.

--- screenlogic/protocol/packet_writer.go
+++ screenlogic/protocol/packet_writer.go
@@ -8,64 +8,64 @@ import (
 )
 
 type PacketWriter struct {
-	w         io.Writer
-	tmpHeader PacketHeader
-	packetBuf bytes.Buffer
+	w        io.Writer
+	sequence uint16
 }
 
 func NewPacketWriter(w io.Writer, startingSequence uint16) *PacketWriter {
 	pw := &PacketWriter{
-		w: w,
+		w:        w,
+		sequence: startingSequence,
 	}
-
-	pw.tmpHeader.Sequence = startingSequence
 
 	return pw
 }
 
 func (pw *PacketWriter) WritePacket(p WriteablePacket) error {
-	// Always reset our buffer
-	defer pw.packetBuf.Reset()
-
 	code, dataBuf, err := p.Encode()
 	if err != nil {
 		return err
 	}
 
-	pw.tmpHeader.TypeID = code
+	var tmpHeader PacketHeader
+	var packetBuf bytes.Buffer
+
+	tmpHeader.TypeID = code
 
 	// Set this packet's length to the *actual* amount of data in its buffer.
 	//
 	// Also um, this is just WAITING for an unexpected uint64 truncation...
 	// ...but we shouldn't see packets larger than UINT32_MAX in this protocol anyway?
-	pw.tmpHeader.Len = 0
+	tmpHeader.Len = 0
 	if dataBuf != nil {
-		pw.tmpHeader.Len = uint32(dataBuf.Len())
+		tmpHeader.Len = uint32(dataBuf.Len())
 	}
+
+	tmpHeader.Sequence = pw.sequence
 
 	// Next we'll copy to a single contiguous buffer so we can write to the network all
 	// at once. The gateway hardware/firmware seems to be sensitive to this, for whatever reason.
 	// First the header...
-	err = binary.Write(&pw.packetBuf, binary.LittleEndian, pw.tmpHeader)
+	err = binary.Write(&packetBuf, binary.LittleEndian, tmpHeader)
 	if err != nil {
 		return err
 	}
 
 	// Increment frame sequence
-	pw.tmpHeader.Sequence++
+	pw.sequence++
 
 	// Now for the actual packet data.
 	if dataBuf != nil {
-		_, err = dataBuf.WriteTo(&pw.packetBuf)
+		_, err = dataBuf.WriteTo(&packetBuf)
 		if err != nil {
 			return err
 		}
 	}
 
-	expectedWriteLen := pw.packetBuf.Len()
+	expectedWriteLen := packetBuf.Len()
 
 	// Now write the whole thing to the network.
-	n, err := pw.packetBuf.WriteTo(pw.w)
+	n, err := packetBuf.WriteTo(pw.w)
 	if err != nil {
 		return err
 	}
